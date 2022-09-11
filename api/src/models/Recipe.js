caf25178354b6db9e05bc135d454eceec0f2c7cd
Auriarte20 <uriarte2001alexis@gmail.com>
2022-09-10 21:03:10 -0500

    Add data base and relations

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    summary: { // Resumen del plato*
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    healthScore: { // Nivel de "comida saludable"
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    steps: { // Paso a paso
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: true,
     },
   });
 };
