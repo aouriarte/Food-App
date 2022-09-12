c9ee28632430ca9721a86210a58ad16aa8a7c9f5
Auriarte20 <uriarte2001alexis@gmail.com>
2022-09-11 20:19:48 -0500

    Add changes required

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Landing from './components/Landing';
+import Home from './components/Home';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/home' component={Home} />
+      </Switch>
     </div>
   );
 }
