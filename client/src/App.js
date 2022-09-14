0a3f65bdd462d7ffe6fc7c8d36e584e124e39a5b
Auriarte20 <uriarte2001alexis@gmail.com>
2022-09-14 09:45:30 -0500

    Add changes required

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import Home from './components/Home';
+import CardDetails from './components/CardDetails';
+import CreateRecipe from './components/CreateRecipe';
 
 import './App.css';
 
-
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/home' component={Home} />
-      </Switch>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/recipe/:id' component={CardDetails} />
+          <Route exact path='/create' component={CreateRecipe} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
