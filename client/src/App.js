820bf1d871fa4afc7c806d34d1dec65b7c6ec36b
Auriarte20 <uriarte2001alexis@gmail.com>
2022-09-24 20:52:22 -0500

    Add changes required

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './components/Landing';
-import Home from './components/Home';
-import CardDetails from './components/CardDetails';
-import CreateRecipe from './components/CreateRecipe';
+import { Route, Switch } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
+import Home from './components/Home/Home';
+import CardDetails from './components/CardDetails/CardDetails';
+import CreateRecipe from './components/CreateRecipe/CreateRecipe';
 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/home' component={Home} />
-          <Route exact path='/recipe/:id' component={CardDetails} />
-          <Route exact path='/create' component={CreateRecipe} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/recipe/:id' component={CardDetails} />
+        <Route exact path='/create' component={CreateRecipe} />
+      </Switch>
+    </div>
   );
 }
 
